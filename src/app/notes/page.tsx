bc0e1b5afca2b3c0b4544b0060770a6c719af23f
Rayne Mallari <raynemllr.5@gmail.com>
2024-02-20 12:51:19 +0800

    feat: initial notes page layout

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,9 +1,19 @@
+import { db } from '@/lib/db/prisma'
+import { auth } from '@clerk/nextjs'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'NextNote - Notes'
 }
 
-export default function NotesPage() {
-  return <div>NotesPage</div>
+export default async function NotesPage() {
+  const { userId } = auth()
+
+  if (!userId) {
+    throw new Error('userId undefined')
+  }
+
+  const allNotes = await db.note.findMany({ where: { userId } })
+
+  return <div>{JSON.stringify(allNotes)}</div>
 }
