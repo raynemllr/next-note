229358e6a0e6cd5be41368fed4adfbf9e6d4ce1b
Rayne Mallari <raynemllr.5@gmail.com>
2024-02-20 12:49:43 +0800

    feat(NavBar): update layout elements and styles

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -1,39 +1,53 @@
+'use client'
+
+import AddNoteDialog from '@/components/AddNoteDialog'
 import { Button } from '@/components/ui/button'
 import { UserButton } from '@clerk/nextjs'
 import { Plus } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
 
 const NavBar = () => {
+  const [shadowAddNoteDialog, setShowAddNoteDialog] = useState(false)
+
   return (
-    <div className='p-4 shadow'>
-      <div className='m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3'>
-        <Link href='/notes' className='flex items-center gap-1'>
-          <Image
-            src='/next-note-logo.png'
-            alt='NextNote logo'
-            width={40}
-            height={40}
-          />
+    <>
+      <div className='p-4 shadow'>
+        <div className='m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3'>
+          <Link href='/notes' className='flex items-center gap-2'>
+            <Image
+              src='/next-note-logo.png'
+              alt='NextNote logo'
+              width={40}
+              height={40}
+            />
 
-          <span className='font-bold'>NextNote</span>
-        </Link>
+            <span className='font-bold'>NextNote</span>
+          </Link>
 
-        <div className='flex items-center gap-2'>
-          <UserButton
-            afterSignOutUrl='/'
-            appearance={{
-              elements: { avatarBox: { width: '2.5rem', height: '2.5rem' } }
-            }}
-          />
+          <div className='flex items-center gap-2'>
+            <UserButton
+              afterSignOutUrl='/'
+              appearance={{
+                elements: { avatarBox: { width: '2.5rem', height: '2.5rem' } }
+              }}
+            />
 
-          <Button>
-            <Plus size={20} className='mr-2' />
-            Add Note
-          </Button>
+            <Button onClick={() => setShowAddNoteDialog(true)}>
+              <Plus size={20} className='mr-2' />
+              Add Note
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* Benefit of having open and setOpen is it doesn't clear the input if the component was unmounted */}
+      <AddNoteDialog
+        open={shadowAddNoteDialog}
+        setOpen={setShowAddNoteDialog}
+      />
+    </>
   )
 }
 export default NavBar
