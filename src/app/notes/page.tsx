c2c182a844c96b0cef962cd9f001301a89dc5bdf
Rayne Mallari <raynemllr.5@gmail.com>
2024-02-21 14:37:09 +0800

    feat: display all the notes using the Note component

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,3 +1,4 @@
+import Note from '@/components/Note'
 import { db } from '@/lib/db/prisma'
 import { auth } from '@clerk/nextjs'
 import { Metadata } from 'next'
@@ -15,5 +16,17 @@ export default async function NotesPage() {
 
   const allNotes = await db.note.findMany({ where: { userId } })
 
-  return <div>{JSON.stringify(allNotes)}</div>
+  return (
+    <div className='grid gap-3 sm:grid-cols-2 lg:grid-cols-3'>
+      {allNotes.map(note => (
+        <Note note={note} key={note.id} />
+      ))}
+
+      {allNotes.length === 0 && (
+        <div className='col-span-full text-center'>
+          You don&apos;t have any notes yet. Why don&apos;tyou create one?
+        </div>
+      )}
+    </div>
+  )
 }
