deed5d02080026247dd29e0f9e1e54a994f24b1f
Rayne Mallari <raynemllr.5@gmail.com>
2024-02-21 14:36:54 +0800

    feat: add Note component to display each note in a card layout style

---
+++ src/components/Note.tsx
@@ -0,0 +1,37 @@
+import { Note as NoteModel } from '@prisma/client'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+
+type NoteProps = {
+  note: NoteModel
+}
+
+const Note = ({ note }: NoteProps) => {
+  const wasUpdated = note.updatedAt > note.createdAt
+
+  const createdUpdatedAtTimestamp = (
+    wasUpdated ? note.updatedAt : note.createdAt
+  ).toDateString()
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{note.title}</CardTitle>
+        <CardDescription>
+          {createdUpdatedAtTimestamp}
+          {wasUpdated && ' (updated)'}
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <p className='whitespace-pre-line'>{note.content}</p>
+      </CardContent>
+    </Card>
+  )
+}
+export default Note
