6bd574f6ce084971cf69e7fc5274c1dc3bb9d0e4
Rayne Mallari <raynemllr.5@gmail.com>
2024-02-21 17:57:31 +0800

    feat: update Note card component

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Note as NoteModel } from '@prisma/client'
 import {
   Card,
@@ -6,32 +8,49 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
+import { useState } from 'react'
+import AddEditNoteDialog from './AddEditNoteDialog'
 
 type NoteProps = {
   note: NoteModel
 }
 
 const Note = ({ note }: NoteProps) => {
+  const [showEditDialog, setShowEditDialog] = useState(false)
+
   const wasUpdated = note.updatedAt > note.createdAt
 
-  const createdUpdatedAtTimestamp = (
+  const createdUpdatedAtDate = (
     wasUpdated ? note.updatedAt : note.createdAt
   ).toDateString()
 
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{note.title}</CardTitle>
-        <CardDescription>
-          {createdUpdatedAtTimestamp}
-          {wasUpdated && ' (updated)'}
-        </CardDescription>
-      </CardHeader>
+  const updatedAtTimestamp = note.updatedAt.toLocaleTimeString()
 
-      <CardContent>
-        <p className='whitespace-pre-line'>{note.content}</p>
-      </CardContent>
-    </Card>
+  return (
+    <>
+      <Card
+        className='cursor-pointer transition-shadow hover:shadow-lg'
+        onClick={() => setShowEditDialog(true)}
+      >
+        <CardHeader>
+          <CardTitle>{note.title}</CardTitle>
+          <CardDescription>
+            {createdUpdatedAtDate}
+            {wasUpdated && ` at ${updatedAtTimestamp} (updated)`}
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <p className='whitespace-pre-line'>{note.content}</p>
+        </CardContent>
+      </Card>
+
+      <AddEditNoteDialog
+        open={showEditDialog}
+        setOpen={setShowEditDialog}
+        noteToEdit={note}
+      />
+    </>
   )
 }
 export default Note
