ffa75e9065dedb4f69846fc31018208be45d30d5
Rayne Mallari <raynemllr.5@gmail.com>
2024-02-22 14:54:02 +0800

    feat: integrate ThemeToggleButton and change the logo file to svg for dark mode

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import AddEditNoteDialog from '@/components/AddEditNoteDialog'
+import ThemeToggleButton from '@/components/ThemeToggleButton'
 import { Button } from '@/components/ui/button'
 import { UserButton } from '@clerk/nextjs'
 import { Plus } from 'lucide-react'
@@ -17,7 +18,7 @@ const NavBar = () => {
         <div className='m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3'>
           <Link href='/notes' className='flex items-center gap-2'>
             <Image
-              src='/next-note-logo.png'
+              src='/next-note-logo.svg'
               alt='NextNote logo'
               width={40}
               height={40}
@@ -26,13 +27,15 @@ const NavBar = () => {
             <span className='font-bold'>NextNote</span>
           </Link>
 
-          <div className='flex items-center gap-2'>
+          <div className='flex items-center gap-4'>
             <UserButton
               afterSignOutUrl='/'
               appearance={{
                 elements: { avatarBox: { width: '2.5rem', height: '2.5rem' } }
               }}
             />
+
+            <ThemeToggleButton />
 
             <Button onClick={() => setShowAddNoteDialog(true)}>
               <Plus size={20} className='mr-2' />
