1e01c4fd6cc8a17cce82a11e95c66912395a04c6
Rayne Mallari <raynemllr.5@gmail.com>
2024-02-28 15:50:50 +0800

    feat: add min height screen to notes layout

--- src/app/notes/layout.tsx
+++ src/app/notes/layout.tsx
@@ -6,9 +6,9 @@ export default function NotesLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
+    <div className='min-h-screen'>
       <NavBar />
       <main className='mx-auto max-w-7xl p-4'>{children}</main>
-    </>
+    </div>
   )
 }
