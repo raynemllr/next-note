3d7819de123141bd375a985acaf121e2ceaf1ffb
Rayne Mallari <raynemllr.5@gmail.com>
2024-02-28 20:08:53 +0800

    fix: redirect after sign out

--- src/app/notes/layout.tsx
+++ src/app/notes/layout.tsx
@@ -1,10 +1,18 @@
 import NavBar from '@/app/notes/NavBar'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
 export default function NotesLayout({
   children
 }: {
   children: React.ReactNode
 }) {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect('/')
+  }
+
   return (
     <div className='min-h-screen'>
       <NavBar />
